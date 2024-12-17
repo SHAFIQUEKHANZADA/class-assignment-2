82454cc2f55b6e0fbc809ea8a8c6e26dabb5b4c3
Shafique Ur Rehman <kzshafique77@gmail.com>
2024-12-17 18:18:15 +0500

    Completed

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import AdvCourses from "@/components/AdvanceCourses";
+import CoreCourses from "@/components/CoreCourses";
+import Hero from "@/components/Hero";
+import Vision from "@/components/Vision";
+ 
+export default function Home() {
+  return (
+     <div>
+        <Hero/>
+        <Vision/>
+        <CoreCourses/>
+        <AdvCourses/>
+     </div>
+  );
+}
